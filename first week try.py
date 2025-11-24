f8cf91bb2286592b3f98bb272e86ea742f89cba1
shywatermelon <1287060257@qq.com>
2025-11-24 13:41:15 +0800

    Initial commit: Add week1 projects

---
+++ first week try.py
@@ -0,0 +1,15 @@
+celsius = 30
+fahrenheit = (celsius* 9/5) + 32
+print(fahrenheit)
+关键在于 if-elif-else 的 “排他性” 和 “顺序性”。它只会执行它遇到的第一个为 True 的分支。
+
+这就是为什么在写这类条件判断时，条件的顺序非常重要。通常，我们会把最严格、范围最窄的条件放在最前面，然后依次放宽，这样才能保证逻辑的正确性。
+score = 80
+if score>=90:
+    print("优秀")
+elif score>=80:
+    print("良好")
+elif score>=70:
+    print("一般")
+else:
+    print("不及格")
---
+++ first week.py
@@ -0,0 +1,22 @@
+print("hello world")
+message ="hello world again"
+print(message)
+age = 24
+print(age)
+pi = 3.14159365358
+print(pi)
+
+name = "迟远方"
+# 使用 f-string 格式化字符串，非常常用！
+welcom = f"你好, {name}！欢迎来到Python的世界。"
+print (welcom)
+fruits = ["apple","banana","orange"]
+print(fruits[0])
+fruits.append("peach")
+print(fruits)
+# if语句
+my_age = 12
+if my_age>=18:
+   print("old")
+else:
+   print("young") 
